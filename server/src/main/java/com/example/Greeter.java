67808163f13c7e4b50fecaaf90d8e2fd4c8f51d6
Bindhu <bichin57@in.ibm.com>
2018-05-23 19:28:52 +0530

    removed SOP

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    System.out.println("Hello");
     return String.format("Hello, %s!", someone);
   }
 }
